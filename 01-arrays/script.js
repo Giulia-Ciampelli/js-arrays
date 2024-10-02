77e4763b832376c5ea843ad1f70587b93b023ba8
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-02 15:12:43 +0200

    docs: solve fourth problem

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -24,7 +24,9 @@ const lastTeacher = teachers;
 console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+teachers.shift();
+const firstTeacher = teachers;
+console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
